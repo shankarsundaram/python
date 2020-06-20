e183fca26da2267d9dd980eaf913361c57da4d5e
shankarsundaram <shank.iam88@gmail.com>
2020-06-20 11:26:09 +0000

    clean code

--- SRE/example_metrics.py
+++ SRE/example_metrics.py
@@ -1,31 +1,41 @@
 from calendar import monthrange
 from reliablemetrics import ReliableMetrics
 
+# sre object
 sre = ReliableMetrics()
 
+# sre uptime
 var_1 = sre.uptime(2040,2)
 print(var_1)
 
+#sre downtime
 var_2 = sre.downtime(10)
 print(var_2)
 
+#sre sla
 var_3 = sre.sla()
 print(var_3)
 
+# sre slo
 var_4 = sre.slo()
 print(var_4)
 
+# sre sli
 var_5 = sre.sli()
 print(var_5)
 
+# sre availability
 var_6 = sre.availability(var_1,var_2)
 print(var_6)
 
+# sre get all metrics
 var_7 = sre.get_all_metrics()
 print(var_7)
 
+# sre set all metrics
 var_8 = sre.set_all_metrics(year=202,month=12,downtime=1,sla=99.99,slo=99.99,sli=0,mtbf=10,mttr=60)
 print(var_8)
 
+# sre get error budget
 var_9 = sre.error_budget(99.99)
 print(var_9)
--- SRE/reliablemetrics.py
+++ SRE/reliablemetrics.py
@@ -12,82 +12,64 @@ class ReliableMetrics:
     """    
     def error_budget(self, slo=0, dec_point=3):
         """ Calculate the error budget """
-
         self.slo = slo
         self.dec_point = dec_point
         err_budget = round(float(100-slo),dec_point)
-        
         return err_budget
 
-    def uptime(self,year=int(datetime.today().year),month=int(datetime.today().month)):
+    def uptime(self, year=int(datetime.today().year), month=int(datetime.today().month)):
         """ Calculate uptime in minutes"""
-        
         list_month = []
         self.year = year
         self.month = month 
-        
         month_in_days = list(monthrange(year,month))
         
         for m in month_in_days:
             list_month.append(int(m))
-        
+    
         up_time = list_month[1] * 24 * 60
-        
         return up_time
 
-    def downtime(self,down_time=0):
+    def downtime(self, down_time=0):
         """ Calculate downtime in minutes """
-        
         self.down_time = down_time
-        
         return down_time
     
-    def sla(self,customer_sla=99.99):
+    def sla(self, customer_sla=99.99):
         """ Calculate service level agreement """
-        
         self.customer_sla = customer_sla
-        
         return customer_sla
     
-    def slo(self,objective=99.99):
+    def slo(self, objective=99.99):
         """ Calculate service level objectives """
-        
         self.objective = objective
-        
         return objective
     
-    def sli(self,indicator=0):
+    def sli(self, indicator=0):
         """ Calculate service level indicator """
-        
         self.indicator = indicator
-        
         return indicator
     
-    def availability(self,up_time=100,down_time=0,dec_point=3):
+    def availability(self, up_time=100, down_time=0, dec_point=3):
         """ Calculate availability in %  
         input: uptime , downtime, results with number of decimal points
         """
-        
         self.up_time = up_time
         self.down_time = down_time
         self.dec_point = dec_point
-        
-        avail_percentage = round(float((up_time/(up_time+down_time))*100),dec_point)
-        
+        avail_percentage = round(float((up_time/(up_time+down_time))*100),dec_point)        
         return avail_percentage
     
-    def mtbf(self,up_time,failures=1,dec_point=3):
+    def mtbf(self, up_time, failures=1, dec_point=3):
         """ Calculate mean time between failures """
-        
         self.up_time = up_time
         self.failures = failures
         self.dec_point = dec_point
         mt_bf = round(float(up_time / failures),dec_point)
         return mt_bf
     
-    def mttr(self,down_time,failures=1,dec_point=3):
+    def mttr(self, down_time, failures=1, dec_point=3):
         """ Calculate mean time to repair """
-        
         self.down_time = down_time
         self.failures = failures
         self.dec_point = dec_point
@@ -106,9 +88,7 @@ class ReliableMetrics:
         avail_percentage = self.availability()
         mt_bf = self.mtbf(up_time)
         mt_tr = self.mttr(down_time)
-        
         list_results = [up_time,down_time,customer_sla,objective,indicator,avail_percentage,mt_bf,mt_tr]
-        
         return list_results
 
     def set_all_metrics(self,**kwargs):
